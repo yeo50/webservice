dc58e4e9f3087e28bc0582a8897c272eb8bf2e32
yeo50 <minhtet3294@gmail.com>
2023-12-10 18:30:38 +0630

    finished

--- js/script.js
+++ js/script.js
@@ -15,7 +15,12 @@ else {
 }
 })
 
-
+$('.accordion-header').click(function() {
+    $('.accordion .accordion-body').slideUp();
+    $(this).next('.accordion-body').slideDown();
+    $('.accordion .accordion-header span').text('+');
+    $(this).children('span').text('-');
+})
 
 
 
